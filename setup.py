6022ae6330d7b3956aef11b608277c28fbc2d208
Omri Rozenzaft <omri.rozenzaft@gm.com>
2020-03-03 12:58:26 +0200

    make it work with setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
+    package_xml_path='./package.xml',
     packages=['rospy_message_converter'],
     package_dir={'' : 'src'},
 )
